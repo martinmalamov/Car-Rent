377eb7bd363d0d62882e0f8a5883bd28b6358a32
martinmalamov <47690979+martinmalamov@users.noreply.github.com>
2020-11-10 20:08:55 +0200

    add confirm delete

---
+++ handlers/rent/deleteFunc.js
@@ -0,0 +1,13 @@
+var deleteLinks = document.querySelectorAll('.delete');
+
+for (var i = 0; i < deleteLinks.length; i++) {
+  deleteLinks[i].addEventListener('click', function(event) {
+      event.preventDefault();
+
+      var choice = confirm(this.getAttribute('data-confirm'));
+
+      if (choice) {
+        window.location.href = this.getAttribute('href');
+      }
+  });
+}
