3dd69b4e347ac7ceda1d0332751e56022688ade1
martinmalamov <47690979+martinmalamov@users.noreply.github.com>
2020-11-13 22:36:38 +0200

    add login username and password verifycation

--- handlers/users/index.js
+++ handlers/users/index.js
@@ -1,6 +1,7 @@
 const User = require('./User')
 const jwt = require('../../utils/jwt')
 const { cookie } = require('../../config/config')
+const bcrypt = require('bcrypt')
 
 module.exports = {
     get: {
@@ -16,7 +17,7 @@ module.exports = {
             req.user = null
             res.clearCookie(cookie).redirect('/home/')
         },
-        UserInfo(req , res ,next) {
+        UserInfo(req, res, next) {
             const name = req._id
             console.log('Req body fullname from get', name)
         }
@@ -28,18 +29,91 @@ module.exports = {
         login(req, res, next) {
             const { email, password } = req.body
             console.log('Req body', req.body)
-           
+
             User.findOne({ email }).then((user) => {
                 return Promise.all([user.passwordsMatch(password), user])
-            }).then(([match, user]) => {
-                if (!match) {
-                    next(err) //TODO
+            }).then(async ([match, user]) => {
+                // console.log('MATCH', match)
+                // console.log('USER ', user)
+                // console.log('USER body password', password)
+                // console.log('USER hashed password', user.password)
+
+                const validPassword = await bcrypt.compare(req.body.password, user.password);
+                if (!validPassword) {
+                    // return res.status(400).send('Invalid Email or Password.')
+                    res.render('users/login.hbs', {
+                        message: 'username or password is not valid !',
+                        oldInput: { email, password }
+                    })
                     return
+                    // return res.json({ success: false, message: 'passwords do not match' });
+                } else {
+                    const token = jwt.createToken(user)
+                    res.status(201).cookie(cookie, token, { maxAge: 3600000 }).redirect('/rent/shared-rent')
                 }
 
-                const token = jwt.createToken(user)
+                // const ValidateSalt = async ()  =>  {
+                //     const salt = await bcrypt.genSalt(11)
+                //     const hashedPassword = await bcrypt.hash('shhhhaNO' , salt)
+                //     // const hashedPassword = await bcrypt.hash(password, salt)
+                //     console.log('Hashed login', hashedPassword)
+                //     return hashedPassword
+                // }
+                // bcrypt.compare(req.body.password, 'shhhhaNO', function (err) {
+                //     console.log('Password', password)
+                //     console.log('user password', user.password)
+                //     if (ValidateSalt() !== user.password) {
+                //         return res.json({ success: false, message: 'passwords do not match' });
+                //     } else {
+                //         const token = jwt.createToken(user)
+                //         return res.status(201).cookie(cookie, token, { maxAge: 3600000 }).redirect('/rent/shared-rent')
+                //     }
+                // })
 
-                res.status(201).cookie(cookie, token, { maxAge: 3600000 }).redirect('/rent/shared-rent')
+                // bcrypt.compare(password, 'shhhhaNO', function (err , res) {
+                //     if (err) {
+                //         console.log(err)
+                //     }
+                //     if (res) {
+                //         const token = jwt.createToken(user)
+                //         res.status(201).cookie(cookie, token, { maxAge: 3600000 }).redirect('/rent/shared-rent')
+                //     }
+                //     else {
+                //         // response is OutgoingMessage object that server response http request
+                //         return res.json({ success: false, message: 'passwords do not match' });
+                //     }
+                // })
+
+                // if (ValidateSalt() !== user.password) {
+                //     res.render('users/login.hbs', {
+                //         message: 'Password do not match!',
+                //         oldInput: { email, password }
+                //     })
+
+                //     return
+                // }
+
+
+                // if (password !== user.password) {
+                //     alert('Password don\'t match')
+                // }
+                // else if (password === '') {
+                //     alert('Blank password')
+                // }
+                // if (!match) {
+                //     // next(err) //TODO
+                //     return next('Incorrect login or password'); 
+                // }
+
+                // const token = jwt.createToken(user)
+
+                // res.status(201).cookie(cookie, token, { maxAge: 3600000 }).redirect('/rent/shared-rent')
+            }).catch((err) => {
+                res.render('users/login.hbs', {
+                    message: 'username or password is not valid !',
+                    oldInput: { email, password }
+                })
+                return
             })
         },
 
